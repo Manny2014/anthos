d57ce869420b6184602367565755065224463e6f
Emmanuel Rodriguez <erog2013@gmail.com>
2022-02-07 23:29:53 -0600

    Added storage bucket

--- gcloud/run.sh
+++ gcloud/run.sh
@@ -2,3 +2,17 @@
      --membership=$CLUSTER_NAME \
      --config=app-spec.yaml \
      --project=$PROJECT_ID
+
+
+# CONFIG CONNECTOR
+gcloud iam service-accounts create config-connector --project $PROJECT_ID
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:config-connector@$PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/owner"
+
+gcloud iam service-accounts add-iam-policy-binding \
+config-connector@$PROJECT_ID.iam.gserviceaccount.com \
+--member="serviceAccount:$PROJECT_ID.svc.id.goog[cnrm-system/cnrm-controller-manager]" \
+--role="roles/iam.workloadIdentityUser" \
+--project $PROJECT_ID

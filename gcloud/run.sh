a7b7fc8a49b1c99769474c92efa0db81bb40bcc4
Emmanuel Rodriguez <erog2013@gmail.com>
2022-02-07 22:09:54 -0600

    Updated stuff

--- gcloud/run.sh
+++ gcloud/run.sh
@@ -1,4 +1,4 @@
- gcloud alpha container hub config-management apply \
+ gcloud beta container hub config-management apply \
      --membership=$CLUSTER_NAME \
      --config=app-spec.yaml \
      --project=$PROJECT_ID
